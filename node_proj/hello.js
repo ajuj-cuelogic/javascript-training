6b683b40a91fe4020ce44daa777953c8102d5a67
ajuj-cuelogic <aju.john@cuelogic.co.in>
2015-09-15 14:55:35 +0530

    Removed commented code from hello.js

--- node_proj/hello.js
+++ node_proj/hello.js
@@ -10,15 +10,13 @@ app.set('view engine','ejs');
 app.set('views',__dirname+"/views");
 
 var g_items = [
-				{id:1, name:"Javascript"},
-				{id:2, name:"PHP"},
-				{id:3, name:"Jquery"},
-				{id:4, name:"MySql"}
-		];
+				{	id:1, name:"Javascript"},
+				{	id:2, name:"PHP"},
+				{	id:3, name:"Jquery"},
+				{	id:4, name:"MySql"}
+			];
 app.post('/add',function(req, res){
 		var name = req.body.language;
-		//res.send("language added as : "+name);
-		var langs = req.body.oldLang;
 		g_items.push(
 			{	
 				id 	: 	g_items.length+1,
@@ -26,7 +24,7 @@ app.post('/add',function(req, res){
 			});
 			 
 		res.redirect('/aju');
-	});
+});
 app.get('/aju',function(req, res){
 	res.render('index', {
 		title	: 	'Aju John'	,
@@ -34,30 +32,5 @@ app.get('/aju',function(req, res){
 	});
 });
 app.listen(8080, function(){
-	console.log('ready on port 8080')
-})
-
-/*var fileSystem = require('fs');
-fileSystem.readFile('exp.txt', function(err, data){
-	if(err) throw err;
-	var text = data.toString();
-	//console.log(txt); 
-	var results = {};
-
-// Break up the file into lines.
-  var lines = text.split('\n');
-  
-lines.forEach(function(line) {
-    var parts = line.split(' ');
-    var letter = parts[1];
-    var count = parseInt(parts[2]);
-    
-if(!results[letter]) {
-      results[letter] = 0;
-    }
-    
-results[letter] += parseInt(count);
-  });
-  
-console.log(results);
-});*/
+	console.log('ready on port 8080');
+});
